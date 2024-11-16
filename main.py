baeedb0aa7056bed169f240069841381f4561a78
JonatanFD <acunacorahuajon@gmail.com>
2024-11-16 10:59:27 -0500

    some fixes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import *
 import json
 from data.constantes import *
 from algorithm.journey import *
+from start import *
 
 app = Flask(__name__)
 
@@ -14,16 +15,24 @@ def hello_world():
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,POST')
     return response
 
 
 @app.route("/cities")
 def getCities():
-    with open(CITIES_JSON_FILE, 'r') as file:
-        cities = json.load(file)
-        return jsonify(cities)
+
+    try:
+        with open(CITIES_JSON_FILE, 'r') as file:
+            cities = json.load(file)
+            return jsonify(cities)
+    except:
+        start()
+        with open(CITIES_JSON_FILE, 'r') as file:
+            cities = json.load(file)
+            return jsonify(cities)
 
 
 @app.route("/cities/filter")
@@ -34,13 +43,16 @@ def filterCities():
     with open(CITIES_JSON_FILE, 'r') as file:
         cities = json.load(file)
 
-    filtered_cities = [city for city in cities if city['country'].lower() in lower_filters]
-    set_cityids = {f"{city['city']},{city['country']}".lower() for city in filtered_cities}
+    filtered_cities = [
+        city for city in cities if city['country'].lower() in lower_filters]
+    set_cityids = {f"{city['city']},{city['country']}".lower()
+                   for city in filtered_cities}
 
     with open(ROUTES_JSON_FILE, 'r') as file:
         routes = json.load(file)
 
-    filtered_routes = [route for route in routes if route['from'].lower() in set_cityids or route['to'].lower() in set_cityids]
+    filtered_routes = [route for route in routes if route['from'].lower(
+    ) in set_cityids or route['to'].lower() in set_cityids]
 
     return jsonify({
         "cities": filtered_cities,
@@ -50,13 +62,19 @@ def filterCities():
 
 @app.route("/routes")
 def getRoutes():
-    with open(ROUTES_JSON_FILE, 'r') as file:
-        routes = json.load(file)
-        return jsonify(routes)
-    
+    try:
+        with open(ROUTES_JSON_FILE, 'r') as file:
+            routes = json.load(file)
+            return jsonify(routes)
+    except:
+        start()
+        with open(ROUTES_JSON_FILE, 'r') as file:
+            routes = json.load(file)
+            return jsonify(routes)
+
 
 @app.route("/journey", methods=['POST'])
 def getJourney():
     data = request.json
-    journey = createJourney(data)    
-    return  jsonify(journey)
+    journey = createJourney(data)
+    return jsonify(journey)
--- start.py
+++ start.py
@@ -3,10 +3,10 @@ from data.routes import *
 from data.graph import *
 
 ### Ejecutar esto para cargar los datos de las ciudades y las rutas
-def main():
+def start():
     mercator()
     no_mercator()
     createRoutes()
     getGraph()
 
-main()
+start()
